041f4f4f09b7030a48b2cfceb680136a3415b6d6
Conner Martin <connerjm2@gmail.com>
2021-04-30 19:59:53 -0700

    Adding react files.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App()
+{
+    return (
+        <div className="wrapper">
+            <h1 className="title">Employee Directory</h1>
+        </div>
+    );
+}
+
+export default App;

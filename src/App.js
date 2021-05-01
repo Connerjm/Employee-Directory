4353a3d0c08c1b5d387f6f7ac70c6ce3cbb4a292
Conner Martin <connerjm2@gmail.com>
2021-04-30 20:22:17 -0700

    Added css and employee card code.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import EmployeeCard from "./components/EmployeeCard";
 
 function App()
 {
     return (
         <div className="wrapper">
             <h1 className="title">Employee Directory</h1>
+            <EmployeeCard
+                firstName="Conner"
+                lastName="Martin"
+                phone="253-389-1831"
+                email="skatercraze@gmail.com"
+                thumbnail=""
+            />
         </div>
     );
 }

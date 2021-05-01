0bbae2ef9df421d80e7286d88155c2e9ad44404b
Conner Martin <connerjm2@gmail.com>
2021-04-30 21:55:59 -0700

    Starting to impliment random employees api. Having trouble getting the entries into the components.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import EmployeeCard from "./components/EmployeeCard";
+import getEmployees from "./utils/API";
 
-function App()
+class App extends Component
 {
-    return (
-        <div className="wrapper">
-            <h1 className="title">Employee Directory</h1>
-            <EmployeeCard
-                firstName="Conner"
-                lastName="Martin"
-                phone="253-389-1831"
-                email="skatercraze@gmail.com"
-                thumbnail=""
-            />
-        </div>
-    );
+    state = { employees: null };
+
+    async setEmployees()
+    {
+        console.log((await getEmployees(20)).data.results);
+        //this.setState({ employees: (await getEmployees(20)).data.results });
+    }
+
+    render()
+    {
+        this.setEmployees();
+        return (
+            <div className="wrapper">
+                <h1 className="title">Employee Directory</h1>
+                {this.state.employees.map(employee => (
+                    <EmployeeCard
+                        firstName={employee.name.first}
+                        lastName={employee.name.last}
+                        phone={employee.phone}
+                        email={employee.email}
+                        thumbnail={employee.picture.thumbnail}
+                    />
+                ))}
+            </div>
+    );}
 }
 
 export default App;

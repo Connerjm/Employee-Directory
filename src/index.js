e84873b566153e0d12362b0f415930a7d2e0cdad
Conner Martin <connerjm2@gmail.com>
2021-05-04 00:55:58 -0700

    Deploying to heroku.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+//Imports
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+//React entry point
 ReactDOM.render(<App />, document.getElementById("root"));

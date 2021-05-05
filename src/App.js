d89901c7c42d355ea3a793bb15cc9c7088d14ec8
Conner Martin <connerjm2@gmail.com>
2021-05-04 23:05:59 -0700

    Fleshed out the sort and filter functions and added them to dropdown buttons. Pretty much done.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,65 @@
 //Imports
 import React, { Fragment, useEffect, useState } from "react";
+import { DropdownButton, Dropdown, Button } from 'react-bootstrap';
 import EmployeeCard from "./components/EmployeeCard";
 import getEmployees from "./utils/API";
 
 //Defining funcional component.
 export default function App()
 {
+    //Original employee array.
+    const [original, setOriginal] = useState([]);
     //Employee array state.
     const [employees, setEmployees] = useState([]);
 
     //Use effect on component mount.
     useEffect(() =>
     {
+        //Get random employees into an array.
         getEmployees(20)//Get 20 employees for now.
-            .then(res => { setEmployees(res.data.results); })
+            .then(res => { setOriginal(res.data.results); setEmployees(res.data.results); console.log(res.data.results); })
             .catch(err => { console.error(err); });
     //eslint-disable-next-line
     }, []);
 
     //Sort option to sort by employee name.
-    function sortByName()//TODO: Add choice between ascending and descening.
+    function sortByName(direction)//TODO: Add choice between ascending and descening.
     {
-        setEmployees([...employees.sort((e1, e2) => e1.name.first.localeCompare(e2.name.first))]);
+        if (direction === 1)
+            setEmployees([...employees.sort((e1, e2) => e1.name.first.localeCompare(e2.name.first))]);
+        else
+            setEmployees([...employees.sort((e1, e2) => e2.name.first.localeCompare(e1.name.first))]);
     }
 
     //Filter option to filter by employee title.
     function filterByTitle(title)//TODO: Change to use title.
     {
-        setEmployees(employees.filter(employee => employee.name.title === "Mr"));
+        setEmployees(original.filter(employee => employee.name.title === title));
+    }
+
+    //Return to the original array.
+    function reset()
+    {
+        setEmployees(original);
     }
 
     //Return to render the component.
     return (
         <Fragment>
             <h1 className="title">Employee Directory</h1>
-            <button className="btn btn-primary" onClick={sortByName}>Sort by Name</button>
-            <button className="btn btn-primary" onClick={filterByTitle}>Filter by Gender</button>
+            <div className="buttons">
+                <DropdownButton title="Sort by">
+                    <Dropdown.Item onClick={() => sortByName(1)}>Name Ascending</Dropdown.Item>
+                    <Dropdown.Item onClick={() => sortByName(-1)}>Name Descending</Dropdown.Item>
+                </DropdownButton>
+                <DropdownButton title="Filter by">
+                    <Dropdown.Item onClick={() => filterByTitle("Mr")}>Title: Mr</Dropdown.Item>
+                    <Dropdown.Item onClick={() => filterByTitle("Ms")}>Title: Ms</Dropdown.Item>
+                    <Dropdown.Item onClick={() => filterByTitle("Mrs")}>Title: Mrs</Dropdown.Item>
+                    <Dropdown.Item onClick={() => filterByTitle("Miss")}>Title: Miss</Dropdown.Item>
+                </DropdownButton>
+                <Button onClick={reset}>Reset</Button>
+            </div>
             <div className="wrapper">
                 {employees.map((employee, index) => (
                     <EmployeeCard
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,7 @@
+//Imports
 import axios from "axios";
 
+//Export single api call function.
 export default function getEmployees(howMany)
 {
     return axios.get(`https://randomuser.me/api/?results=${howMany}&inc=name,email,phone,picture&nat=us`);
